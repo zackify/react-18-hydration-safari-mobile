fa864020852b0ba01c589c599aade4cefb5e41c2
Zach Silveira <zackify@gmail.com>
2023-02-21 20:06:16 -0500

    commit demo

---
+++ app/routes/index.tsx
@@ -0,0 +1,38 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Welcome to Remix</h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+        <section className="copyright-section pt-8 pr-8 text-base font-normal leading-6 text-brand-gray-7">
+          hi
+          <br />
+          this has a br and then a phone number: 1-800-GAMBLER. hydration cant
+          handle this
+        </section>
+      </ul>
+    </div>
+  );
+}
